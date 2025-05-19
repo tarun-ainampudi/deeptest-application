fc0614a1e4232acaa095efd454d0bb4277695125
tarun-ainampudi <tarunainampudi@gmail.com>
2025-05-19 11:06:05 +0530

    removed unnecessary files

--- model_change.py
+++ model_change.py
@@ -13,6 +13,20 @@ operator = ""
 model = otaversion.split("_")[0]
 romversion = otaversion.split("_")[0] + "_" +  otaversion.split("_")[1].split(".")[0] + "_" + otaversion.split("_")[1].split(".")[1] + "." + otaversion.split("_")[1].split(".")[2] 
 
-# CHANGE THE VALUES HERE TO CHANGE THE MODEL
-# CHANGE THE VALUES HERE TO CHANGE THE MODEL
-# CHANGE THE VALUES HERE TO CHANGE THE MODEL 
+# Example values to replace in the above code:
+
+# model: e.g., RMX3461
+
+# otaVersion: e.g., RMX3461_11.C.20_1200_202408131148
+
+# operator: null (or replace with an actual value if available)
+
+# romVersion: e.g., RMX3461_11_C.20
+
+# colorOSVersion: e.g., V12.1
+
+# androidVersion: e.g., 12
+
+# trackRegion: e.g., null (or replace with an actual value if available)
+
+# uRegion: e.g., null (or replace with an actual value if available)
